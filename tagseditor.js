f5512ce420090829f3cd24f0298cac1b5be3ece4
serfire <serfire@gmail.com>
2014-06-09 18:00:41 +0800

    init editor

---
+++ tagseditor.js
@@ -0,0 +1,20 @@
+(function($) {
+
+  $.fn.tagsEditor = function(userOptions) {
+    var $self = this,
+      inputs = new Array(),
+      options,
+      defaultOptions,
+      cursor = 0;
+    defaultOptions = {
+      width: '300px',
+      height: '100px',
+    };
+    options = $.extend(true, {}, defaultOptions, userOptions);
+
+    buildEditor = function() {
+
+    }
+  };
+
+})(jQuery);
