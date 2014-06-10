38aa760660eb7dd58cb548eb98ceae378f36144b
serfire <serfire@gmail.com>
2014-06-11 06:43:32 +0800

    N/A

--- tagseditor.js
+++ tagseditor.js
@@ -6,8 +6,20 @@
 
     TagsEditor = function(id) {
 
-      addTag = function(val){
-        alert(val)
+      interfaceMarkup = function(id, settings){
+        var markup = '<div id="' + id + '-tags-editor" class="tagseditor"><div id="' + id + '-addTag">';
+    if (settings.interactive) {
+      markup = markup + '<input id="' + id + '-tag" value="" data-default="' + settings.defaultText + '" />';
+    }
+    markup = markup + '</div><div class="tags-clear"></div></div>';
+    return markup;
+      };
+
+      addTag = function(val, index){
+        if(index == undefined){
+          index = data.cursor;
+        }
+
       };
 
       importTags = function(vals){
@@ -18,6 +30,8 @@
           })
         }
       };
+
+
 
       var $self = this,
         inputs = new Array(),
@@ -36,20 +50,24 @@
         tags: new Array(),
         pid: id,
         real_input:'#' + id,
-        tag_wraper
+        input_wrapper:'#' + id + '-addTag',
+        fake_input: '#' + id + '-tag',
       };
-      var value = $(data.real_input).val();
+
+
+            init = function(){
+var value = $(data.real_input).val();
+      $(interfaceMarkup(id, options)).insertAfter($('#' + id));
       if(value != undefined){
         importTags(value);
       }
+      };
 
-      
-
-
+      init();
 
     };
 
-this.each(function() {
+  this.each(function() {
       var id = $(this).attr('id');
       var editor = new TagsEditor(id);
     });
